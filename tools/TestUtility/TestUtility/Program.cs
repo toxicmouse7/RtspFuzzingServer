80e54dc81743b42dd867c712cd1d397877762c72
Алексей Гладков <aleksej.s.gladkov@gmail.com>
2025-02-01 01:05:56 +0500

    save

---
+++ tools/TestUtility/TestUtility/Program.cs
@@ -0,0 +1,29 @@
+﻿using Flurl;
+using Microsoft.AspNetCore.SignalR.Client;
+
+var apiAddress = Environment.GetEnvironmentVariable("API_ADDRESS");
+
+var fuzzingHub = new HubConnectionBuilder()
+    .WithUrl(apiAddress.AppendPathSegment("fuzzing_hub"))
+    .Build();
+
+var totalPacketsToSend = 0;
+var packetsSent = 0;
+    
+fuzzingHub.On<int>("PreFuzz", val =>
+{
+    totalPacketsToSend = val;
+});
+
+fuzzingHub.On<int>("PacketSent", val =>
+{
+    packetsSent = val;
+});
+
+fuzzingHub.On("Error", () =>
+{
+    Console.Error.WriteLine("Failed");
+    Environment.Exit(-1);
+});
+
+await fuzzingHub.StartAsync();
